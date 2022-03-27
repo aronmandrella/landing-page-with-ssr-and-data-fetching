ba2401b68de94ac7bbcf14b10017f8d1d4a901ac
Aron <aron.mandrella@gmail.com>
2022-03-27 18:19:13 +0200

    gh pages paths fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
+const GH_PAGE_SLUG = "/landing-page-with-ssr-and-data-fetching";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  /*
+    GitHub pages store website files under url that looks like this:
+    https://username.github.io/repo-name/
 
-module.exports = nextConfig
+    But Next.js vt default prepares absolute assets urls that expect that these assets are here:
+    https://username.github.io/
+  */
+  basePath: GH_PAGE_SLUG,
+};
+
+module.exports = nextConfig;
